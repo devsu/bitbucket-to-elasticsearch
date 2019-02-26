c709456caea4af5f7e9386e9d748c50d32e384d3
Sebastian Guerrero <msebgf@yahoo.com>
2019-02-26 14:20:21 -0500

    add user email and project info to commits

--- src/sync.js
+++ src/sync.js
@@ -53,7 +53,7 @@ module.exports = class Sync {
     }
 
     await Promise.all([
-      this.synchronizeCommits(repo.slug, minDate),
+      this.synchronizeCommits(repo.slug, minDate, repo.project),
       this.synchronizeRefs(repo.slug),
     ]);
     // Save the repo only after all its commits and refs have been processed.
@@ -72,7 +72,7 @@ module.exports = class Sync {
     );
   }
 
-  async synchronizeCommits(repoSlug, minDate) {
+  async synchronizeCommits(repoSlug, minDate, project) {
     const commitsIterator = this.bitbucket.getCommitsIterator(repoSlug, minDate);
     const promises = [];
     for await (const data of commitsIterator) {
@@ -85,7 +85,7 @@ module.exports = class Sync {
       }
       const nodes = commits.map((commit) => commit.hash);
       promises.push(...nodes.map((node) => this.synchronizeStatuses(repoSlug, node)));
-      promises.push(this.database.saveCommits(Sync.transformCommits(commits)));
+      promises.push(this.database.saveCommits(Sync.transformCommits(commits, project)));
     }
     await Promise.all(promises);
   }
@@ -184,9 +184,10 @@ module.exports = class Sync {
     return data.map(Sync.transformRepository);
   }
 
-  static transformCommit(data) {
+  static transformCommit(data, project) {
     const transformed = Object.assign({}, data);
     delete transformed.links;
+    transformed.project = this.transformProject(project);
     if (transformed.author.user) {
       delete transformed.author.user.links;
       transformed.author.user.email = this.parseEmail(transformed.author.raw);
@@ -209,8 +210,8 @@ module.exports = class Sync {
     return transformed
   }
 
-  static transformCommits(data) {
-    return data.map(Sync.transformCommit);
+  static transformCommits(data, project) {
+    return data.map( (commitsData) => Sync.transformCommit(commitsData, project));
   }
 
   static transformStatus(data) {
@@ -233,6 +234,7 @@ module.exports = class Sync {
     delete transformed.default_merge_strategy;
     delete transformed.merge_strategies;
     transformed.target = Sync.transformCommit(transformed.target);
+    delete transformed.target.project;
     if (transformed.tagger && transformed.tagger.user) {
       delete transformed.tagger.user.links;
     }
--- src/sync.spec.js
+++ src/sync.spec.js
@@ -67,7 +67,10 @@ describe('BitbucketSync', () => {
 
   describe('transformCommit()', () => {
     test('should transform bitbucket data to elastic search data', () => {
-      const actual = BitbucketSync.transformCommit(bitbucketCommitData);
+      const actual = BitbucketSync.transformCommit(
+        bitbucketCommitData,
+        bitbucketRepositoryData.project
+      );
       expect(actual).toEqual(esCommitData);
     });
 
@@ -76,14 +79,19 @@ describe('BitbucketSync', () => {
       delete bitbucketCommitDataCopy.author.user;
       const esCommitDataCopy = Object.assign({}, esCommitData);
       delete esCommitDataCopy.author.user;
-      const actual = BitbucketSync.transformCommit(bitbucketCommitDataCopy);
+      const actual = BitbucketSync.transformCommit(
+        bitbucketCommitDataCopy,
+        bitbucketRepositoryData.project
+      );
       expect(actual).toEqual(esCommitDataCopy);
     });
   });
 
   describe('transformCommits()', () => {
     test('should transform bitbucket data to elastic search data', () => {
-      const actual = BitbucketSync.transformCommits([bitbucketCommitData, bitbucketCommitData]);
+      const actual = BitbucketSync.transformCommits(
+        [bitbucketCommitData, bitbucketCommitData],
+        bitbucketRepositoryData.project);
       expect(actual).toEqual([esCommitData, esCommitData]);
     });
   });
